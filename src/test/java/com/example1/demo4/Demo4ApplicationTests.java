dee148f07b4887d3e320b9a4204519f4cceb3dc1
group007 <pranaliWankhade94@gmail.com>
2019-06-14 22:45:45 +0530

    first commit

--- src/test/java/com/example1/demo4/Demo4ApplicationTests.java
+++ src/test/java/com/example1/demo4/Demo4ApplicationTests.java
@@ -1,12 +1,9 @@
 package com.example1.demo4;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.testng.annotations.Test;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+
+
 public class Demo4ApplicationTests {
 
 	@Test
